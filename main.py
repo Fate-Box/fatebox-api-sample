5e0291a3dcd7749d83a8d9c029262003a73100e6
kayu0514 <kunka6066@gmail.com>
2024-11-13 15:51:23 +0900

    initial commit.

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+# 名前を入れる
+name = "kayu0514"
+
+# API
+api = f"https://fate-box-api.onrender.com/divination/{name}"
+
+req = requests.get(api)
+
+if req.status_code == 200:
+    data = req.json()
+    names = data["name"]
+    divi = data["divination"]
+
+    print(f"名前 : {names}\n結果 : {divi}")
+else:
+    print(f"HTTPエラーが発生しました: {req.status_code}")
